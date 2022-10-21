930dedbc82e18ff88d205726a07a4e0a6133a3cf
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-10-21 14:26:17 +0300

    merging

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :projects
   mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'auth'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # resources :projects
   # mount_devise_token_auth_for 'User', at: 'auth'
