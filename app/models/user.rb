93985d94732a018c74a5f9dca1f231e25431d04f
Patrick <ruomungai@gmail.com>
2022-10-23 10:37:01 +0300

    add enum with defined user roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  enum role: [:user, :admin]
+
+  after_initialize do
+  if self.new_record?
+    self.role ||= :user
+  end
+end
+
   has_many :projects
   has_many :members
 end
