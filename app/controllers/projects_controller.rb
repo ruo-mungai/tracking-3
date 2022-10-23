b8bba67b4d51e2eb8e2a2ce5c050836a2d0c1654
Patrick <ruomungai@gmail.com>
2022-10-23 11:52:47 +0300

    get all projects associated to a user

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -46,6 +46,11 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def my_project
+    @project= current_user.projects
+    render json: @project
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
