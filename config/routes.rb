30b3dbe8852d6c5c4a47b795e2962ec43d2c5571
Patrick <ruomungai@gmail.com>
2022-10-23 11:55:31 +0300

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :members
   resources :projects
   mount_devise_token_auth_for 'User', at: 'auth'
+  #  get all individual user projects
+  get 'myprojects', to: 'projects#my_project'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # resources :projects
   # mount_devise_token_auth_for 'User', at: 'auth'
